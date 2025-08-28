ab46175e0caa340f5fa7d4e198144c5e47a06c07
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-08-28 10:57:58 +0500

    push

--- single.js
+++ single.js
@@ -1,15 +1,15 @@
-// import http from "http";
+import http from "http";
 
-// http.createServer((req,res)=>{
-//     res.writeHead(200);
-//     res.end(`Handled by single  process \n`);
-// }).listen(3000);
-// console.log(`single process server running on 3000 pid ${process.pid}`);
+http.createServer((req,res)=>{
+    res.writeHead(200);
+    res.end(`Handled by single  process \n`);
+}).listen(3000);
+console.log(`single process server running on 3000 pid ${process.pid}`);
 
 
 
-import os from "os";
+// import os from "os";
 
-const totalCPU = os.cpus().length;
+// const totalCPU = os.cpus().length;
 
-console.log(`Total CPU cores available: ${totalCPU}`);
+// console.log(`Total CPU cores available: ${totalCPU}`);
